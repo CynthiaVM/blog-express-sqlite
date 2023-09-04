4e10faf80231197e4b108988aa5d065cd27e87d4
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-09-04 02:16:19 -0300

    Update server.ts
    Iniciamos base de datos con Heidi

--- server.ts
+++ server.ts
@@ -1,15 +1,30 @@
 import express, { Express, Request, Response } from "express";
 import bodyParcer from 'body-parser';
+import noticiasRoutes from './Modules/noticias/noticia.routes';
+import { dbcontext } from './Modules/db/dbcontext';
+
+dbcontext
+	.initialize()
+	.then(() => {
+		console.log('Base datos en orden!');
+	})
+	.catch((err) => {
+		console.error('Base datos DOWN', err); //si hay error
+	});
+
 
 const app: Express = express();
 
 app.use(bodyParcer.json());
 
-app.use('/', (req: Request, res: Response) => {
-    res.json({
-        mgs:'Servisor funcionando ok!',
-    });
-});
+//app.use('/', (req: Request, res: Response) => {
+    //res.json({
+       // mgs:'Servisor funcionando ok!',
+    //});
+//});
+
+app.use('/noticia', noticiasRoutes);
+
 app.listen(3000, () => {
     console.log('Servidor funcionando ok!!!');
 });
