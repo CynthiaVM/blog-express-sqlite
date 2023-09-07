bd474c05202f4a9696390a794df4a5dd4ee47b0f
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-09-07 02:53:59 -0300

    Create comentario.entity.ts
    Creamos comentarios

---
+++ Modules/comentarios/comentario.entity.ts
@@ -0,0 +1,17 @@
+import { Column,CreateDateColumn, Entity, PrimaryGeneratedColumn, ManyToOne} from 'typeorm';
+import { Noticia } from '../noticias/noticia.entity';
+
+@Entity() //decorador agregando detalles
+export class Comentario { //propiedad que va a tener el comentario
+	@PrimaryGeneratedColumn('uuid') //dice que sera una primarykey (identificador unico)
+	id: string; //el signo de pregunta es por que puede o no venir
+
+	@Column() //decorador
+	comentario: string;
+
+	@CreateDateColumn()
+	create_at: Date;
+
+    @ManyToOne(() => Noticia, (n) => n.comentarios, { nullable: false }) //la funcion es para q pueda bindear con la propiedad que va a estar del otro lado.
+	noticia: Noticia; 
+ }
