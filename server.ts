c850013bf9b720a027a3d767f7d0a78a92a8cdeb
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-09-20 01:04:04 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -5,23 +5,29 @@ import { dbcontext } from './Modules/db/dbcontext';
 import comentarioRoutes from './Modules/comentarios/comentario.routes';
 import { logMiddleware } from "./Modules/middleware/logMiddleware";
 import logger from './Modules/logger/logger';
+import { TypeORMError } from 'typeorm';
+import dotenv from 'dotenv';
 process.env.TZ = 'America/Argentina/Buenos_Aires';
+dotenv.config();
+
+
+
 const time = new Date();
 console.log(time.toLocaleDateString());
 
 
 dbcontext
-	.initialize()
-	.then(() => {
-		console.log('Base datos en orden!');
-	})
-	.catch((err) => {
-		console.error('Base datos DOWN', err); //si hay error
-	});
+.initialize()
+.then(() => {})
+.catch((err: TypeORMError) => {
+	logger.error(`Error al iniciar la base de datos: ${err.message}`); // si hay error
+});
 
 
 const app: Express = express();
 //mi primer middleware global
+const PORT = process.env.BLOG_PORT;
+
 app.use(logMiddleware);
 
 app.use(bodyParcer.json());
@@ -36,6 +42,6 @@ app.use('/noticia', noticiasRoutes);
 
 app.use('/comentario', comentarioRoutes);
 
-app.listen(3000, () => {
-	logger.info('Servidor funcionando OK EN EL PORT ' + 3000);
+app.listen(PORT, () => {
+	logger.info('Servidor funcionando OK EN EL PORT ' + PORT);
 });
