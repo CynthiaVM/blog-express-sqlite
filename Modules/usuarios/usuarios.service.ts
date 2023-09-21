46bfe95cfd4c8cbd7185979e6c93479240a3f688
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-09-20 21:14:35 -0300

    Create usuarios.service.ts

---
+++ Modules/usuarios/usuarios.service.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { usuarios } from "./usuarios.entity";
+import { dbcontext } from "../db/dbcontext";
+import { IUsuario } from "./usuarios.interface";
+import logger from "../logger/logger";
+
+export const crearUsuario =async (req: Request, res: Response) => {
+    try {
+        const usuarioRepository= dbcontext.getRepository(usuarios);
+        let usuarioData: IUsuario= req.body;
+        usuarioData.email=usuarioData.email.toLowerCase();
+        const usuario= await usuarioRepository.create(usuarioData);
+        const guardarUsuario= await usuarioRepository.save(usuario);
+
+        res.json({
+			msg: `Se creo el usuario con el id: ${guardarUsuario.id}`,
+		});
+
+    } catch (error) {
+        logger.error(`Fallo crear el usuario ${error}`);
+		res.status(500).json({ msg: 'No se pudo guardar el usuario'});
+        
+    }
+    
+}
