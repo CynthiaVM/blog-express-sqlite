cb1a81e808c2be0efcdb8d2390d24d73a7d95930
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-09-28 02:42:37 -0300

    Create auth.middleware.ts

---
+++ Modules/auth/auth.middleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+
+export const verifyTokenMiddleware = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const token = req.headers['authorization']; //validarlo
+
+	if (!token) {
+		return res.status(403).json({ msg: 'Token no proporcionado' });
+	}
+
+	// TODO: verificar que el token
+	// decode y enviar la informacion a la siguiente
+
+	next();
+};
