60a407fa09191c375bd25412f7eeac1cd12de64e
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-10-05 02:22:39 -0300

    Update auth.service.ts

--- Modules/auth/auth.service.ts
+++ Modules/auth/auth.service.ts
@@ -5,6 +5,8 @@ import { Ilogin } from './auth.interfaces';
 import bcrypt from 'bcrypt';
 import logger from '../logger/logger';
 import { generarTokenJWT } from './jwt.service';
+import { IUsuario } from '../usuarios/usuarios.interface';
+import { IjwtPayload } from './jwt.interfaces';
 
 
 export const login = async (req: Request, res: Response) => {
@@ -30,31 +32,27 @@ export const login = async (req: Request, res: Response) => {
 			throw new Error('Usuario/contraseña incorrecto');
 		}
 
-		// Genero token
-			const payload = {
-			usuario: {
-				id_usuario: buscarUsuario.id,
-				email: buscarUsuario.email,
-			    nombre: buscarUsuario.nombre,
-				apellido: buscarUsuario.apellido,
-				},
-			};
-	
-			const token = generarTokenJWT(payload);
-	
-			res.json({
-				token: token,
-			});
 
-		//res.json({
-			//msg: `El resultado del login fue : ${compararPass}`,
-		//});
+		// Genero token
+		const payload: IjwtPayload = {
+				usuario: {
+					id: buscarUsuario.id,
+					email: buscarUsuario.email,
+					nombre: buscarUsuario.nombre,
+					apellido: buscarUsuario.apellido,
+					},
+		};
+
+		const token = generarTokenJWT(payload);
+
+		res.json({
+			token: token,
+		});
 	} catch (error) {
 		// implementar logging en modo ERROR
-		//throw new Error('Usuario/contraseña incorrecto');
 		logger.error(error);
 		res.status(401).json({
 			msg: 'Usuario/contraseña incorrecto',
-		});	
+		});
 	}
 };
