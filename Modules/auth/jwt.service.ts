5585bdf8ebbc3b7b2219cc813315514168828858
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-10-05 02:23:14 -0300

    Update jwt.service.ts

--- Modules/auth/jwt.service.ts
+++ Modules/auth/jwt.service.ts
@@ -1,13 +1,11 @@
 import jwt from 'jsonwebtoken';
-import logger from '../logger/logger';
+import { IjwtPayload } from './jwt.interfaces';
+
+
 
 const secret = process.env.SECRET_JWT || 'DefaultPassword'; //llamamos nuestra clave
 
-export const generarTokenJWT = (payload: object): string => {
-	const token = jwt.sign(payload, secret, { expiresIn: '2h' }); //sign es firmar, payload, clave, tiempo de duracion token
+export const generarTokenJWT = (payload: IjwtPayload): string => {
+	const token = jwt.sign(payload, secret, { expiresIn: '2h' }); // tiempo que dura el token
 	return token;
 };
-
-// export const verificarToken = (token:string): token => {
-
-// }
