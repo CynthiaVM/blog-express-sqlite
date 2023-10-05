56dd786730cc04e8e73b4ccc4fa1ae8c1f23fdeb
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-10-05 02:22:36 -0300

    Update auth.middleware.ts

--- Modules/auth/auth.middleware.ts
+++ Modules/auth/auth.middleware.ts
@@ -1,18 +1,30 @@
 import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import logger from '../logger/logger';
+
+const secret = process.env.SECRET_JWT || 'DefaultPassword';
 
 export const verifyTokenMiddleware = (
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) => {
-	const token = req.headers['authorization']; //validarlo
+	const token = req.headers['authorization']?.replace('Bearer ', ''); //validarlo
 
 	if (!token) {
 		return res.status(403).json({ msg: 'Token no proporcionado' });
 	}
 
-	// TODO: verificar que el token
-	// decode y enviar la informacion a la siguiente
-
-	next();
+	//verificar que el token
+	jwt.verify(token, secret, (err: any, decoded: any) => {
+		if (err) {
+			logger.error(err);
+			return res.status(401).json({ msg: 'Cuidado, Token no valido!' });
+		}
+		//agregarlo al request 
+		logger.debug(JSON.stringify(decoded));
+		req.usuario = decoded.usuario;
+		next();
+	});
+	
 };
